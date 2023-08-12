821268fe4dd2670851618e2b011509cd00525ad8
Anthony-D99 <anthonydisaf@yahoo.com>
2023-08-12 13:51:32 -0400

    attemtping to get service worker to function properly.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,6 +20,8 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new WorkboxPlugin.GenerateSW(),
+
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE'
